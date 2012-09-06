76d58f9e37a6dda9740182b8f6a5d3fb76108b42
Alexander Schrode <midix01@googlemail.com>
2012-09-06 22:16:13 +0200

    Some changes

--- src/database.py
+++ src/database.py
@@ -1,32 +1,32 @@
 '''
     Database.py
-    
-    @brief Handles Database represation and Database handling 
+
+    @brief Handles Database represation and Database handling
     @author Alexander Schrode
     @date   16.07.2012
     @mail   midix01[at]googlemail.com
 
     @attention: sqlite converters are not award of malicous code
                 but it should need for this kind of software.
-    
+
     Copyright (c) 2012 Alexander Schrode
 
-    Permission is hereby granted, free of charge, to any person obtaining a 
+    Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
     to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-    and/or sell copies of the Software, and to permit persons to whom the 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
     Software is furnished to do so, subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in 
+    The above copyright notice and this permission notice shall be included in
     all copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
     DEALINGS IN THE SOFTWARE.
 
 '''
@@ -38,7 +38,7 @@ class Title:
    def __init__(self, title, artist, length, filename, bpm=0.0, key='', genre=''):
        '''Inits a title'''
        self.title    = title
-       self.artists  = artist
+       self.artist  = artist
        self.length   = length
        self.genre    = genre
        self.bpm      = bpm
@@ -46,7 +46,7 @@ class Title:
        self.filename = filename
 
    def __str__(self):
-       return "%s;%s;%d;%f;%d;%s;%s" % (self.text, self.artist, self.length
+       return "%s;%s;%d;%f;%s;%s;%s" % (self.title, self.artist, self.length,
                                         self.bpm, self.key, self.genre, self.filename)
 
 class CD:
@@ -56,7 +56,7 @@ class CD:
         self.tracks = zip[titles, artists]
 
     def __str__(self):
-        tracks, artists = [x[0], x[1] for x in self.tracks]
+        tracks, artists = [[x[0], x[1]] for x in self.tracks]
         return "%s;%s;%s" % (self.name, '#'.join(tracks), '#'.join(artists))
 
 def title_adapter(title):
@@ -65,6 +65,7 @@ def title_adapter(title):
 
 def title_converter(s):
     '''converts title sqlite type to a Python Title Class'''
+    print(s)
     s = s.split(";")
     title, artist = s[0], s[1]
     length, key = int(s[2]), int(s[4])
@@ -77,7 +78,7 @@ def cd_adapter(cd):
     '''converts CD class for sqlite3'''
     return str(cd)
 
-def title_converter(s):
+def cd_converter(s):
     '''converts cd sqlite type to a Python CD Class'''
     s = s.split(";")
     return CD(s[0], s[1].split('#'), s[2].split('#'))
@@ -85,6 +86,9 @@ def title_converter(s):
 
 class Database:
     ''' Database Adapter for all transactions '''
+
+    self.titel_keys = ['name', 'artist', 'key', 'bpm', 'genre', 'file']
+
     def __init__(self, filename):
         self._register_adapter()
         self._connect(filename)
@@ -99,29 +103,44 @@ class Database:
 
     def _connect(self, filen):
         '''Loads data base from @filen'''
-        if isfile(filen):
-            self._db = sqlite3.connect(filen)
-        else:
-            self._db = sqlite3.connect(filen)
-            cur = self._db.cursor()
-            cur.execute("creat table title_db(t title)")
-            cur.execute("creat table cd_db(c cd)")
-            cur.close()
+        self._db = sqlite3.connect(filen)
+        cur = self._db.cursor()
+        cur.execute("create table if not exists title_db(t title)")
+        cur.execute("create table if not exists cd_db(c cd)")
+        cur.close()
 
     def insert_titles(self, titles):
         '''Insert all elements of an list/tupel of titles into DB'''
         cur = self._db.cursor()
-        cur.execute("insert into title_db(t) values (?)",
-                    (x for x in titles))
+        for elem in titles:
+           cur.execute("insert into title_db(t) values (?)",
+                       (elem,))
+        self._db.commit()
         cur.close()
 
     def insert_cd(self, cd):
         '''Insert all elements of an list/tupel of cds into DB'''
         cur = self._db.cursor()
-        cur.execute("insert into cd_db(c) values (?)",
-                    (x for x in cd))
+        for elem in cd:
+           cur.execute("insert into cd_db(c) values (?)", (elem,))
+        self._db.commit()
         cur.close()
 
+    def get_all_titles(self):
+       '''Get all Titles from DB'''
+       cur = self._db.cursor()
+       cur.execute('select t as "t [title]" from title_db')
+       ret = cur.fetchall()[0]
+       #@todo add correct handling
+       cur.close()
+       return ret
+'''
+    def get_by_parameter_titel(search, key):
+       if key not in self.titel_keys:
+          pass
+       else:
+          return None
+'''
     def backup_database(target):
         '''Copy a backup to target dir'''
         copyfile(filen, target)
--- src/scanner.py
+++ src/scanner.py
@@ -1,83 +1,84 @@
 '''
     scanner.py
-    
+
     @brief Scanner Classes to process files in the directorys
-    @deps  rely on stagger  
+    @deps  rely on stagger
     @author Alexander Schrode
     @date   16.07.2012
     @mail   midix01[at]googlemail.com
 
     @attention: sqlite converters are not award of malicous code
                 but it should need for this kind of software.
-    
+
     Copyright (c) 2012 Alexander Schrode
 
-    Permission is hereby granted, free of charge, to any person obtaining a 
+    Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
     to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-    and/or sell copies of the Software, and to permit persons to whom the 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
     Software is furnished to do so, subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in 
+    The above copyright notice and this permission notice shall be included in
     all copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
     DEALINGS IN THE SOFTWARE.
 
 '''
 
-
-
 import os
 import stagger
 from stagger.id3 import * # contains ID3 frame types
-from database import Title
+from database import Title, Database
 
 class Base_Scanner:
     files = ''
-    
+
     def process_file(self, file):
         pass
-    
+
     def process_dir_pre(self, dir):
         pass
-    
+
     def process_dir_post(self, dir):
         pass
-    
+
     def scann(self, dir):
         for item in os.listdir(dir):
             file = os.path.join(dir, item)
-            if os.path.isdir(file):
+            if file == dir:
+                pass
+            elif os.path.isdir(file):
                 self.process_dir_pre(dir)
-                self.scann(dir)
+                self.scann(file)
                 self.process_dir_post(dir)
-            elif os.path.isfile(file) and os.path.splitext(item)[1] == self.files:
+            elif os.path.isfile(file) and os.path.splitext(item)[1] in self.files:
                 self.process_file(file)
 
 class Full_Music_Scann(Base_Scanner):
-
+    files = ['.mp3']
     def run(self, dir):
         '''scanns a dir and returns Title objects'''
         self._titles = []
         self.scann(dir)
-        return self._titles 
+        return self._titles
 
     def process_file(self, f):
         track = stagger.read_tag(f)
-        self._titles.append(Title(track.title, track.artist, track[TLEN], 
-                            os.path.join(os.path.basename(f), f), int(track[TBPM]),
-                            track[TKEY], track.genre))
-    
-if __name__ == '__main__':
+        self._titles.append(Title(track.title, track.artist,
+                                  1,#track[TLEN].text[0],
+                                  f, int(track[TBPM].text[0]),
+                                  track[TKEY].text[0],
+                                  track.genre))
 
+if __name__ == '__main__':
     test = Full_Music_Scann()
-    res = test.run() 
-    for x in res:
-        print res
+    res = test.run('C:\\muisk\\beats mp3')
+    #for x in res:
+     #  print(x)
